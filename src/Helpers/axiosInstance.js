0149a17441792f5051baa91835f649c65fadc0ce
Dipu Kumar Gupta <kumardipu845420@gmail.com>
2026-01-30 01:41:05 +0530

    backend url added

--- src/Helpers/axiosInstance.js
+++ src/Helpers/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/api/v1";
+const BASE_URL = "https://lms-learning-management-system-mfok.onrender.com/api/v1";
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
